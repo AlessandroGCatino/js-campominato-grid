0754ce69e367b5fbb5a86b2ee40a6f1575a55943
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-01-16 18:54:24 +0100

    conclusione esercizio no bonus

---
+++ main.js
@@ -0,0 +1,37 @@
+const grigliaHtml = document.getElementById('grid')
+
+document.getElementById('start').addEventListener("click", function () {
+
+    grigliaHtml.innerHTML=""
+    grigliaHtml.style += "border: 2px solid black;"
+    for (let i = 0; i < 100; i++) {
+
+        let square = document.createElement("div")
+        square.classList.add("square")
+        square.innerHTML = `<span>${ i+1 }</span>`
+
+        square.addEventListener('click', function () {
+
+            console.log( this.querySelector( "span" ).innerText )
+            this.classList.toggle("active")
+        })
+
+        grigliaHtml.append(square)
+    }
+})
+
+
+
+
+
+
+
+
+/*Bonus
+Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+
+
+*/
